9328f4c28900554408fe91e68ff1342a26310c58
LeoFlhaus <leoflehinghaus@gmail.com>
2025-03-18 20:10:17 +0100

    Add files via upload

---
+++ FlatSurface/basics.h
@@ -0,0 +1,17 @@
+#ifndef _BASICS_H_
+#define _BASICS_H_
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Define here simulation settings
+// -------------------------------------------------------------------------------------------------------------------------
+#define NP 5000             // Number of cells
+#define PI 3.14159265
+#define v0 3.0              // Initial active force
+#define DT 0.0001           // Integration time step
+
+#define samples 50          // Samples written to file
+#define stepsInSample 50000 // Number of steps to run between each sample
+#define gamma1 1.0          // Turning rate
+#define EPS 1000.0          // Interaction strength
+#define sig 666.7           // Wall strength
+#endif
